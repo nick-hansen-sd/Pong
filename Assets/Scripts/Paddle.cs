c0a287492e7099b5b72ac4674d722bb2f03510f7
nick-hansen-sd <nhansen@csumb.edu>
2026-02-04 16:16:25 -0800

    Arena and paddle movement

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
 
     public float paddleSpeed = 1f;
+    public float maxZ = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,16 +17,27 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         
-        if (Keyboard.current.dKey.isPressed)
+        if ((Keyboard.current.wKey.isPressed && transform.position.x < 0) || (Keyboard.current.upArrowKey.isPressed && transform.position.x > 0))
         {
-            transform.position += new Vector3(0f, 0f, paddleSpeed) * Time.deltaTime;
+            Vector3 newPosition = transform.position + new Vector3(0f, 0f, paddleSpeed) * Time.deltaTime;
+            newPosition.z = Mathf.Clamp(newPosition.z, -10f, maxZ);
+
+            transform.position = newPosition;
+            //transform.position += new Vector3(0f, 0f, paddleSpeed) * Time.deltaTime;
         }
 
-        if (Keyboard.current.aKey.isPressed)
+        if ((Keyboard.current.sKey.isPressed && transform.position.x < 0) || (Keyboard.current.downArrowKey.isPressed && transform.position.x > 0))
         {
             transform.position -= new Vector3(0f, 0f, paddleSpeed) * Time.deltaTime;
         }
 
+        Vector3 up = Vector3.up;
+        Quaternion testRotation = Quaternion.Euler(60f, 0f, 0f);
+
+        Vector3 rotatedVector = testRotation * up;
+
+        Quaternion otherRotation = Quaternion.Euler(-60f, 0f, 0f);
+        Vector3 otherRotatedVector = otherRotation * up;
 
     }
 }
